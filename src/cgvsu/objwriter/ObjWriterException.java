0a471da40b7cef34f2a893da05b9a6156a5ebdbb
Devonty <Devonty@yandex.ru>
2023-12-29 20:21:48 +0300

    Нашел Reader

---
+++ src/cgvsu/objwriter/ObjWriterException.java
@@ -0,0 +1,8 @@
+package cgvsu.objwriter;
+
+public class ObjWriterException extends RuntimeException {
+    public ObjWriterException(String errorMessage) {
+        super("Error in ObjWriter: " + errorMessage);
+    }
+}
+
